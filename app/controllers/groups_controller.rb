c8e5d6a18a388018db0c3a6c32034a2c7aa7217d
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-06-02 17:17:06 -0700

    aadds ability to associate users with group in group form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -37,7 +37,7 @@ class GroupsController < ApplicationController
 	private
 
 	def group_params 
-		params.require(:group).permit(:name, :description, :avatar)
+		params.require(:group).permit(:name, :description, :avatar, :user_ids => [])
 	end
 
 
