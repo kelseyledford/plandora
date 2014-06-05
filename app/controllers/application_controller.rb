c1b84eea91678cc54c4a4602ad03f5d36c59b42f
charlyy <cyoon90@yahoo.com>
2014-06-05 11:08:23 -0700

    fixed groups to show up in navbar and some more styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	
+	before_action :set_groups
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def set_groups
+  	@groups = Group.all
+  end
+
 end
