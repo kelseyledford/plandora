50e4a96276d8a45fd1a8577903ef4be17ff2b0dd
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-06-05 11:10:26 -0700

    adds groups to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_action :set_groups
+
   private
+
+  def set_groups
+  	@groups = Group.all
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
